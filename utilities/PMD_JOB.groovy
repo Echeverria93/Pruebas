3affa64cd76f7e83ce61888a9021404b8c344d15
Echeverria93 <32390767+Echeverria93@users.noreply.github.com>
2018-05-12 23:42:09 -0400

    Add files via upload

--- utilities/PMD_JOB.groovy
+++ utilities/PMD_JOB.groovy
@@ -5,12 +5,21 @@ import javaposse.jobdsl.dsl.Context;
 
 public class PMD_JOB implements Context {
 
+    private String Project_version
+    private def jdk_x
     private String propertiesFile
+    private String Name_Proyect
+    private String Project_Version
+    private String deploy_stage
 	private String ant_home
 	private String fileBuild
-	 private def jdk_x
 
-static void addPMD_WEB_JOB(def job, def jdk_x, String fileBuild, String ant_home, String propertiesFile){
+static void addPMD_WEB_JOB(def job, def jdk_x,String Name_Proyect,String Project_Version, String deploy_stage, String fileBuild, String ant_home, String propertiesFile){
+
+	def prop_stage_build ='''\
+PROJECT_NAME='''+Name_Proyect+'''-'''+Project_Version+'''
+DEPLOY_STAGE= '''+deploy_stage+'''
+'''.stripIndent()
 
 			 job.with{
 			 jdk(jdk_x)
