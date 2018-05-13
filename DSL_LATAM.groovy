44ea07bd39b600cbdefe8af0c9fbed53b6ad186a
Echeverria93 <32390767+Echeverria93@users.noreply.github.com>
2018-05-13 19:06:42 -0400

    Add files via upload

--- DSL_LATAM.groovy
+++ DSL_LATAM.groovy
@@ -154,7 +154,7 @@ folder('Latam' + '/' + Name_Proyect) {
                     upstream('Latam' + '/' + Name_Proyect + '/' + item + '/' + Name_Proyect + '_GIT', 'SUCCESS')
                 }
             }
-            BUILD_JOB.addBUILD_ROBOT(BUILD, jdk_x, propertiesFile, Name_Proyect, Project_Version, deploy_stage, fileBuild, ant_home, item, correoJP)
+            BUILD_JOB.addBUILD_ROBOT(BUILD, jdk_x, propertiesFile, Name_Proyect, Project_Version, deploy_stage, fileBuild, ant_home, item, correoJP, Patch_Workspace)
 
             //---------------------------------------------------------------------------------------------------------------------------------------
 
--- utilities/PMD_JOB.groovy
+++ utilities/PMD_JOB.groovy
@@ -15,6 +15,7 @@ public class PMD_JOB implements Context {
 	private String fileBuild
 	private String correoJP
 	private String item
+	private String Patch_Workspace
 	
 
 static void addPMD_WEB(def job, def jdk_x,String Name_Proyect,String Project_Version, String deploy_stage, String fileBuild, String ant_home, String propertiesFile, String correoJP){
@@ -61,10 +62,9 @@ DEPLOY_STAGE= '''+deploy_stage+'''
 }
 
 
-static void addPMD_ROBOT(def job, def jdk_x,String Name_Proyect,String Project_Version, String deploy_stage, String fileBuild, String ant_home, String propertiesFile,String item){
+static void addPMD_ROBOT(def job, def jdk_x,String Name_Proyect,String Project_Version, String deploy_stage, String fileBuild, String ant_home, String propertiesFile,String item, String Patch_Workspace){
 
-def patch_workspace = "${PATCH_WORKSPACE_JENKINS}"
-def git_beta = patch_workspace + 'Latam' + '/' + Name_Proyect + '/' + item
+def git_beta = Patch_Workspace + 'Latam' + '/' + Name_Proyect + '/' + item
  def shell_PMD_RobotB= '''\
  echo "Ejecutando  PMD 5.0"
  cd /var/jenkins_home/resources/REV_COD/pmd-5.0
