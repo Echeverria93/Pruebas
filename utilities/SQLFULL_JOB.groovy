adf4da60c41da33d1e18aa0708bf9cf9dfbb2e45
Echeverria93 <32390767+Echeverria93@users.noreply.github.com>
2018-05-13 01:14:59 -0400

    Add files via upload

--- utilities/SQLFULL_JOB.groovy
+++ utilities/SQLFULL_JOB.groovy
@@ -18,7 +18,7 @@ private String bd
 private String directorio
 private String intranet
 
-static void addSQLFULL_WEB_JOB(def job, String Name_Proyect, String Project_Version, String item, def tp String user, String pass, String host, String puerto, String bd, String directorio, String intranet){
+static void addSQLFULL_WEB_JOB(def job, String Name_Proyect, String Project_Version, String item, def tp, String user, String pass, String host, String puerto, String bd, String directorio, String intranet){
 
 def shell_sql_full='''\
 echo "Ejecutando  ApiSQL_Scanner.jar"
