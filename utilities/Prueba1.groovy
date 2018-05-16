2554b8915f1e5f3a41268664d52573614d22f4f5
Echeverria93 <32390767+Echeverria93@users.noreply.github.com>
2018-05-15 23:06:18 -0400

    Add files via upload

--- utilities/Prueba1.groovy
+++ utilities/Prueba1.groovy
@@ -3,10 +3,10 @@ package utilities
 import javaposse.jobdsl.dsl.Context;
 
 
-public class Prueba1 {
+public class Prueba1 implements Context {
 
 
-public static void Mauricio(){
+static void Mauricio(){
 
 print "La cadena insertada ha sido texto"
 
